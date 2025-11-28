698d7c1f5be61d9aa706ffb2bd4c03aad3dc2f90
YogenderVermaa <yogender.tech@gmail.com>
2025-11-28 06:33:24 +0530

    added elsecase routers

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Wallet from "../pages/Wallet";
 import Home from "../pages/Home";
 import ProtectedRoute from "./protectedRoute";
@@ -6,26 +6,27 @@ import Upload from "../pages/Upload";
 import Layout from "../Layout";
 import LayoutWithoutHeader from "../LayoutN";
 
-
 export const routes = createBrowserRouter([
+  // Public routes (no header)
+  {
+    element: <LayoutWithoutHeader />,
+    children: [
+      { path: "/", element: <Wallet /> }, // Login page
+    ],
+  },
 
+  // Private routes (with header)
+  {
+    element: <Layout />,
+    children: [
+      { path: "/home", element: <ProtectedRoute><Home /></ProtectedRoute> },
+      { path: "/upload-image", element: <ProtectedRoute><Upload /></ProtectedRoute> },
+    ],
+  },
 
-    {
-        path:"/",
-        element:<LayoutWithoutHeader/>,
-        children:[
-            {
-                path:"/",element:<Wallet/>
-            }
-        ]
-    },
-    {
-        path:'/',
-        element:<Layout/>,
-        children:[
-            {path:'/', element:<Wallet/>},
-            {path:'/home', element:<ProtectedRoute><Home/></ProtectedRoute>},
-            {path:'/upload-image', element:<ProtectedRoute><Upload/></ProtectedRoute>},
-        ]
-    }
-])
+  // Catch all 404
+  {
+    path: "*",
+    element: <LayoutWithoutHeader><h1>404 Not Found</h1></LayoutWithoutHeader>
+  }
+]);
